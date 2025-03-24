2da9161f0661c1cb3ae10a9e4f956072c3b19dd1
Ross <ross.mitchell0408@gmail.com>
2025-03-24 16:12:21 -0400

    Lessons can now be properly marked as complete and displayed as such

--- app/src/main/java/com/example/project_g02/LessonListActivity.kt
+++ app/src/main/java/com/example/project_g02/LessonListActivity.kt
@@ -1,6 +1,7 @@
 package com.example.project_g02
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,12 @@ import com.example.project_g02.singletons.Lessons
 import com.google.gson.Gson
 
 class LessonListActivity : AppCompatActivity() {
+    companion object {
+        private const val PREFS_NAME = "LessonPrefs"
+    }
+
     private lateinit var binding: ActivityLessonListBinding
+    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var gson: Gson
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +30,15 @@ class LessonListActivity : AppCompatActivity() {
         val lessons = Lessons
         gson = Gson()
 
-        val userJson = intent.getStringExtra("userJson")
-        val user = gson.fromJson(userJson, User::class.java)
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        if (!isLogged()) {
+            //If the user somehow ended up here without being logged in, send back to login screen
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        val userJson = sharedPreferences.getString("loggedUser", null)
+        val user = userJson.let { gson.fromJson(it, User::class.java) }
+
         val lessonAdapter =
             LessonAdapter(lessons.lessonList, user.completed) { position ->
                 //This lambda function will run when it receives the clicked position from the Adapter
@@ -45,5 +58,8 @@ class LessonListActivity : AppCompatActivity() {
         )
         Log.d(TAG, "After recycler view")
 
+    }
+    private fun isLogged(): Boolean {
+        return sharedPreferences.getString("loggedUser", null) != null
     }
 }
