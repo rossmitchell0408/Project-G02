f60b70a17566c33e396f26cd4eddcb8506266644
Ross <ross.mitchell0408@gmail.com>
2025-03-24 11:27:23 -0400

    Lessons able to be marked as complete with bugs

--- app/src/main/java/com/example/project_g02/LessonDetailActivity.kt
+++ app/src/main/java/com/example/project_g02/LessonDetailActivity.kt
@@ -1,14 +1,24 @@
 package com.example.project_g02
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project_g02.databinding.ActivityLessonDetailBinding
 import com.example.project_g02.models.Lesson
+import com.example.project_g02.models.User
 import com.google.gson.Gson
 
 class LessonDetailActivity : AppCompatActivity() {
+    companion object {
+        private const val PREFS_NAME = "LessonPrefs"
+    }
+
     private lateinit var binding: ActivityLessonDetailBinding
+    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var gson: Gson
+/*    private lateinit var userJson:String
+    private lateinit var user: User*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +26,21 @@ class LessonDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         gson = Gson()
 
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
         val lessonJson = intent.getStringExtra("lessonJson")
         val lesson: Lesson = gson.fromJson(lessonJson,Lesson::class.java)
         val position = intent.getIntExtra("lessonIndex",-1)
+
+        if (!isLogged()) {
+            //If the user somehow ended up here without being logged in, send back to login screen
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        var userJson = sharedPreferences.getString("loggedUser", null)
+        val user = userJson.let { gson.fromJson(it, User::class.java) }
+/*        userJson = intent.getStringExtra("loggedUser")
+        user = gson.fromJson(userJson, User::class.java)*/
+
 
         binding.tvTitle.text = lesson.name
         binding.tvTime.text = lesson.getTime()
@@ -31,6 +53,23 @@ class LessonDetailActivity : AppCompatActivity() {
     //Intent to open link on btn click (pull link from lesson)
     //
     //Handle what to do if position is somehow -1 (Shouldnt ever happen? Might be more eloquent way of handling this)
+        binding.btnWatch.setOnClickListener {
+            //binding.tvTest.text = position.toString()
+            binding.tvTest.text = user.completed[position].toString()
 
+        }
+        binding.btnComplete.setOnClickListener {
+            user.completed[position] = true
+            val editor = sharedPreferences.edit()
+            editor.putString("loggedUser", gson.toJson(user))
+            editor.apply()
+
+            val intent = Intent(this,LessonListActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+    private fun isLogged(): Boolean {
+        return sharedPreferences.getString("loggedUser", null) != null
     }
 }
